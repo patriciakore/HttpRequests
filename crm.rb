4bc3fd4b1d9d4b3fa9bb2b49e995ca31cc6b9510
Patricia Kore <patricia.kore@yahoo.com>
2015-04-13 00:46:10 -0400

    part 3

--- crm.rb
+++ crm.rb
@@ -3,16 +3,15 @@ require_relative 'rolodex'
 require 'faker'
 require 'sinatra'
 
-$rolodex=Rolodex.new
+@@rolodex=Rolodex.new
 
 20.times do 
 	contact = Contact.new(Faker::Name.first_name,
 			Faker::Name::last_name,
 			Faker::Internet.email,
 			"Good Stuff")
-	$rolodex.add_contact(contact)
+	@@rolodex.add_contact(contact)
 end
-
 
 
 get '/' do
@@ -22,7 +21,7 @@ end
 
 
 get '/contacts' do
-	@contacts = $rolodex.contacts
+	@contacts = @@rolodex.contacts
 
   erb(:contacts)
 end
@@ -31,8 +30,52 @@ get '/contacts/new' do
   erb(:new_contact)
 end
 
+get '/contacts/:id' do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else 
+    raise Sinatra::NotFound
+  end
+end
+
+put "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/edit' do
+    @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    @@rolodex.delete(params[:id].to_i)
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+
+
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  $rolodex.add_contact(new_contact)
+  @@rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
